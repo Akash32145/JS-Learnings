84d5d5eddf6932d0b93159dc3217530e38695224
Akash <115661251+Akash32145@users.noreply.github.com>
2025-07-25 20:11:28 +0000

    conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,60 @@
+let score = "33"
+// console.log(score); 
+// console.log(typeof(score));
+
+
+let scoreInNumber = Number(score); 
+// console.log(scoreInNumber); 
+// console.log(typeof(scoreInNumber)); 
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+let isLoggedIn = "Akash"; 
+let isLoggedIn_inBoolean = Boolean(isLoggedIn);  
+// console.log(isLoggedIn_inBoolean);
+// console.log(typeof(isLoggedIn_inBoolean)); 
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33; 
+let someNumberInString = String(someNumber); 
+// console.log(someNumberInString); 
+// console.log(typeof(someNumberInString)); 
+
+
+/*
+
+                                        Operations
+
+*/
+
+let val = 33; 
+let negval = -33; 
+// console.log(negval); 
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2^3
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"; 
+let str2 = " Akash"; 
+let str = str1 + str2; 
+// console.log(str); // Hello Akash 
+
+
+// console.log("1" + 2); // 12 
+// console.log(1 + "2"); // 12 
+// console.log("1" + 2 + 2); // 122 
+// console.log(1 + 2 + "2"); // 32 

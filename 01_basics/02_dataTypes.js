fecc3b8f5d9f265b8c58b5a0e371cb466a03bd1c
Akash <115661251+Akash32145@users.noreply.github.com>
2025-07-25 19:24:59 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treats all js code as newer version 
+
+let name = "Akash" // string 
+let age = 18 // number 
+let isLoggedIn = false // boolean 
+let state = null // null object 
+
+
+// number => 2 to power 53
+// bigint // number greater than 2^53 range 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "name") // string 
+console.log(typeof null) // object 
